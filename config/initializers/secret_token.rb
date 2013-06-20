6f3d7a893e269cccc75f0fb3fc5c6b0318a1899b
max collinge <maxcoll23@gmail.com>
2013-06-20 15:17:52 -0700

    Hi there, Here is a rails app and status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maxbook::Application.config.secret_token = 'ff3e1b11d518c5329f744b68e3ae6f934708db5e784bef01579b9fa23e27c8409df9fbd63a832ae5ddf28ef125b6227b0f0e09802a951b878b7d367a8ab18d20'
